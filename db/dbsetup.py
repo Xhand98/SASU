49289dd2da950a1a39a52c01065c01a7ed619417
Hendrick <78098509+Xhand98@users.noreply.github.com>
2024-08-26 08:00:26 -0400

    changed directories of db things to /db/...

--- db/dbsetup.py
+++ db/dbsetup.py
@@ -3,14 +3,7 @@ import sqlite3
 from NewSimpleSQL import Database
 
 
-folderName = 'db'
-fileName = 'sasu_users.db'
-
-db_path =os.path.join('db', 'sasu_users.db')
-if not os.path.exists(folderName):
-    os.makedirs(folderName)
-
-connection = sqlite3.connect('./db/example.db')
+connection = sqlite3.connect('./example.db')
 db = Database(connection)
 
 structure1 = {
@@ -19,7 +12,7 @@ structure1 = {
         {"name": "id", "type": "INTEGER PRIMARY KEY AUTOINCREMENT"},
         {"name": "discord_id", "type": "TEXT"},
         {"name": "discord_username", "type": "TEXT"},
-        {"name": "created_at", "type": "TEXT"},  # Use TEXT for datetime
+        {"name": "created_at", "type": "TEXT"},  
         {"name": "updated_at", "type": "TEXT"}
     ]
 }
@@ -28,7 +21,6 @@ structure2 = {
     "name": "steam_accounts",
     "columns": [
         {"name": "id", "type": "INTEGER PRIMARY KEY AUTOINCREMENT"},
-        {"name": "discord_user_id", "type": "INTEGER"},
         {"name": "steam_id", "type": "TEXT"},
         {"name": "steam_username", "type": "TEXT"},
         {"name": "created_at", "type": "TEXT"},
@@ -40,3 +32,5 @@ db.simple_create_table(structure1)
 db.simple_create_table(structure2)
 
 db.close()
+
+print("Your database has been set up. :D")
