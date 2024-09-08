905ab186a05b1637f4f89a317454c42da73a1165
Hendrick <hendrickherrera9@gmail.com>
2024-09-07 20:10:18 -0400

    Fixed: 1 blank line required after class docstring

--- db/dbmanager.py
+++ db/dbmanager.py
@@ -29,6 +29,7 @@ class DatabaseManager:
         Updates user information.
         backup_database(): Creates a backup of the database.
     """
+
     def __init__(self, db_path):
         """
         Initializes a DatabaseManager object.
