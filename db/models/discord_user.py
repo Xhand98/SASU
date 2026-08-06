554cdfe28f4c71111843046d645be63ccd25275c
Hendrick Herrera <hendrickherrera9@gmail.com>
2026-08-05 23:04:00 -0400

    feat: improved models

--- db/models/discord_user.py
+++ db/models/discord_user.py
@@ -2,5 +2,5 @@ from peewee import TextField
 from .base import BaseModel
 
 class DiscordUser(BaseModel):
-    id = TextField(unique=True, null=False),
+    discord_id = TextField(unique=True, null=False)
     username = TextField(null=False)
--- db/models/steam_account.py
+++ db/models/steam_account.py
@@ -3,6 +3,12 @@ from .discord_user import DiscordUser
 from peewee import TextField, ForeignKeyField
 
 class SteamAccount(BaseModel):
-    id = TextField(unique=True, null=False)
-    discord_id = ForeignKeyField(DiscordUser, backref='id', null=False)
+    steam_id = TextField(unique=True, null=False)
+    discord = ForeignKeyField(
+        DiscordUser,
+        backref="steam_accounts",
+        null=False,
+        unique=True,
+        on_delete="CASCADE",
+    )
     username = TextField(unique=False, null=False)
