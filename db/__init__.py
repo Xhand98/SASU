95fa09604f863c6c953e3d0fdaabb45b772e6815
Hendrick Herrera <hendrickherrera9@gmail.com>
2026-08-05 23:03:09 -0400

    feat: redid setup and added __init__ file

---
+++ db/__init__.py
@@ -0,0 +1,7 @@
+from .database import db
+# from .setup import setup_database
+
+__all__ = [
+    "db",
+    "setup_database"
+]
--- db/dbsetup.py
+++ db/dbsetup.py
@@ -1,40 +1,6 @@
-import sqlite3
-from NewSimpleSQL.SimpleSQLite import Database, ID
+from db.database import db
+from db.models import DiscordUser, SteamAccount, Blacklist
 
-
-connection = sqlite3.connect("./db/users.db")
-db = Database(connection)
-
-structure1 = {
-    "name": "discord_users",
-    "columns": {
-        "discord_id": ID(auto_increment=False),
-        "discord_username": {"type": str, "constraints": "NOT NULL"},
-        "created_at": str,
-        "updated_at": str,
-    },
-}
-structure2 = {
-    "name": "steam_accounts",
-    "columns": {
-        "steam_id": ID(auto_increment=False),
-        "discord_user_id": {"type": int, "constraints": "NOT NULL"},
-        "steam_username": {"type": str, "constraints": "NOT NULL"},
-        "created_at": str,
-        "updated_at": str,
-    },
-    "fk": {"discord_user_id": ("discord_users", "discord_id", True)},
-}
-structure3 = {
-    "name": "blacklist",
-    "columns": {
-        "discord_id": ID(auto_increment=False),
-        "banned_at": str,
-    },
-    "fk": {"discord_id": ("discord_users", "discord_id")},
-}
-
-db.complicated_create_tables([structure1, structure2, structure3])
-db.close()
-
-print("Your database has been set up. :D")
+def setup_db():
+    with db:
+        db.create_tables([DiscordUser, SteamAccount, Blacklist])
